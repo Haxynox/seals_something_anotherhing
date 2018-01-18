2f0e9f57100398514862f929563d8956d96df6ca
Ivan Meler <i_ivan@windowslive.com>
2018-01-18 21:35:13 +0100

    Update hidl configs and enable vibratoe service

--- product/sensors.mk
+++ product/sensors.mk
@@ -17,7 +17,8 @@
 # Sensorhub
 PRODUCT_PACKAGES += \
 	android.hardware.sensors@1.0-impl \
-	android.hardware.vibrator@1.0-impl
+	android.hardware.vibrator@1.0-impl \
+	android.hardware.vibrator@1.0-service
 
 # Permissions
 PRODUCT_COPY_FILES += \
