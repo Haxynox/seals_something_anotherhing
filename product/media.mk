d62e8f969f2f03bab62a7aa852085992d058237f
Ivan Meler <i_ivan@windowslive.com>
2018-02-19 21:30:57 +0100

    Update media profiles

--- product/media.mk
+++ product/media.mk
@@ -1,20 +1,21 @@
 DEVICE_PATH := device/samsung/hero-common
 
+# Media profile
 PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	$(DEVICE_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
-	$(DEVICE_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
+	$(DEVICE_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+	$(DEVICE_PATH)/configs/media/media_profiles_V1_0.xml:system/etc/media_profiles_V1_0.xml \
 	$(DEVICE_PATH)/configs/media/somxreg.conf:system/etc/somxreg.conf 
 
-
- PRODUCT_PACKAGES  += \
-	libui_shim \
+# Stagefright
+PRODUCT_PACKAGES += \
+	libstagefright_shim \
 	libExynosOMX_shim \
-	libstagefright_shim
+	libui_shim
 
 # Overriden service definition
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/init/android.hardware.media.omx@1.0-service.rc:system/vendor/etc/init/android.hardware.media.omx@1.0-service.rc
-
+	$(DEVICE_PATH)/configs/init/android.hardware.media.omx@1.0-service.rc:system/vendor/etc/init/android.hardware.media.omx@1.0-service.rc
