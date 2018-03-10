b2bb7ac925b7223fbb28b41a42d332fe11ad1abb
Ivan Meler <i_ivan@windowslive.com>
2018-03-10 11:09:22 +0100

    Revrt Me: Dex updates

--- system_prop.mk
+++ system_prop.mk
@@ -16,29 +16,13 @@
 
 # Dalvik/Art
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sys.fw.dex2oat_thread_count=4 \
-	dalvik.vm.heapstartsize=8m \
-	dalvik.vm.heapgrowthlimit=256m \
-	dalvik.vm.heapsize=512m \
-	dalvik.vm.heaptargetutilization=0.75 \
-	dalvik.vm.heapminfree=2m \
-	dalvik.vm.heapmaxfree=8m
-
+	dalvik.vm.image-dex2oat-threads=4 \
+	dalvik.vm.boot-dex2oat-threads=4 \
+	dalvik.vm.dex2oat-threads=2 
+	
 # Hwui
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.hwui.texture_cache_size=88 \
-	ro.hwui.layer_cache_size=58 \
-	ro.hwui.path_cache_size=16 \
-	ro.hwui.texture_cache_flushrate=0.4 \
-	ro.hwui.shape_cache_size=4 \
-	ro.hwui.gradient_cache_size=2 \
-	ro.hwui.drop_shadow_cache_size=6 \
-	ro.hwui.r_buffer_cache_size=8 \
-	ro.hwui.text_small_cache_width=1024 \
-	ro.hwui.text_small_cache_height=1024 \
-	ro.hwui.text_large_cache_width=4096 \
-	ro.hwui.text_large_cache_height=2048 \
-	ro.hwui.fbo_cache_size=16
+	ro.hwui.texture_cache_flushrate=0.5
 
 # Network
 # Define default initial receive window size in segments.
@@ -49,7 +33,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	media.stagefright.legacyencoder=1 \
 	media.stagefright.less-secure=1 \
-	sys.use_fifo_ui=1 \
 	ro.telephony.mms_data_profile=5 \
 	persist.media.treble_omx=false
 
