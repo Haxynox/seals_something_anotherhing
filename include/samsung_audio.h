52b5518c300154fd13b683c13bfc2c2968555881
Ivan Meler <i_ivan@windowslive.com>
2018-03-16 20:03:01 +0000

    Disable clock callback sync
    Change-Id: Id82f8e28fb51a12d8ab4e0bb545b38519fa24c1e

--- include/samsung_audio.h
+++ include/samsung_audio.h
@@ -49,5 +49,10 @@
 #define SOUND_CAPTURE_HOTWORD_DEVICE 0
 */
 
+/*
+ * ril_set_call_clock_sync() may cause unknown problems with the call-mic.
+ * Uncomment this to disable the support for it.
+ */
+#define DISABLE_CALL_CLOCK_SYNC
 
 #endif // SAMSUNG_AUDIO_H
