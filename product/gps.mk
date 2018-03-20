9130c92ac36de4e4d49160226e453f742e3f3d91
Ivan Meler <i_ivan@windowslive.com>
2018-03-20 09:07:23 +0000

    Revert "Temp: disable gps"
    This reverts commit 81320086b9a21a3dec4dc0def3a1cb16e88b1280.

---
+++ product/gps.mk
@@ -0,0 +1,18 @@
+DEVICE_PATH := device/samsung/hero-common
+
+# Configs
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+	$(DEVICE_PATH)/configs/gps/gps.xml:system/etc/gps.xml
+
+PRODUCT_PACKAGES += \
+	libsensor_shim
+
+# HIDL
+PRODUCT_PACKAGES += \
+	android.hardware.gnss@1.0-impl \
+	android.hardware.gnss@1.0-service
+
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
