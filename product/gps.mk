07ec38ece19a26e5dc75c84ba448e611fc277fe9
Ivan Meler <i_ivan@windowslive.com>
2018-04-08 14:41:40 +0000

    use proper lhd config path
    Change-Id: I499f2c492d66341e22a04ca847655478a105fab9

--- product/gps.mk
+++ product/gps.mk
@@ -4,7 +4,7 @@ DEVICE_PATH := device/samsung/hero-common
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
 	$(DEVICE_PATH)/configs/gps/gps.xml:system/etc/gps.xml \
-	$(LOCAL_PATH)/configs/gps/lhd.conf:system/etc/lhd.conf
+	$(LOCAL_PATH)/configs/lhd.conf:system/etc/lhd.conf
 
 # HIDL
 PRODUCT_PACKAGES += \
