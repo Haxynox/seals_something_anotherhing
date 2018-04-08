e3f3638bc26a81d58b834ece93edd40673a0b028
Ivan Meler <i_ivan@windowslive.com>
2018-04-08 15:49:49 +0200

    Gps updates

--- product/gps.mk
+++ product/gps.mk
@@ -3,15 +3,16 @@ DEVICE_PATH := device/samsung/hero-common
 # Configs
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
-	$(DEVICE_PATH)/configs/gps/gps.xml:system/etc/gps.xml
-
-PRODUCT_PACKAGES += \
-	libsensor_shim
+	$(DEVICE_PATH)/configs/gps/gps.xml:system/etc/gps.xml \
+	$(LOCAL_PATH)/configs/gps/lhd.conf:system/etc/lhd.conf
 
 # HIDL
 PRODUCT_PACKAGES += \
 	android.hardware.gnss@1.0-impl \
 	android.hardware.gnss@1.0-service
+
+PRODUCT_PACKAGES += \
+	libsensor_shim
 
 # Permissions
 PRODUCT_COPY_FILES += \
