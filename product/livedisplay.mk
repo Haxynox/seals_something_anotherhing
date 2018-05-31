205f158fd0edabd4e80c495db2bf4f464e6a307b
ivanmeler <i_ivan@windowslive.com>
2018-05-31 14:20:18 +0000

    Kang livedisplay from zero

--- product/livedisplay.mk
+++ product/livedisplay.mk
@@ -1,7 +1,8 @@
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm \
-    vendor.lineage.livedisplay-V1.0-java
+    vendor.lineage.livedisplay@1.0-service.zero \
+    vendor.lineage.livedisplay-V1.0-java \
+    libzeroutils
 
 PRODUCT_BOOT_JARS += \
     vendor.lineage.livedisplay-V1.0-java
