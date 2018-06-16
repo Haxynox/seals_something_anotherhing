e379ced641a0f953daed0407dbf2c6fca1520569
ivanmeler <i_ivan@windowslive.com>
2018-06-16 04:34:53 -0700

    Add custom gps impl

---
+++ board/gps.mk
@@ -0,0 +1,3 @@
+# Shims: GPSD
+TARGET_LD_SHIM_LIBS += \
+    /system/bin/gpsd|/system/vendor/lib64/libsensor_shim.so
--- product/gps.mk
+++ product/gps.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/samsung/hero-common
 
 # HIDL
 PRODUCT_PACKAGES += \
-	android.hardware.gnss@1.0-impl \
+	android.hardware.gnss@1.0-impl.hero \
 	android.hardware.gnss@1.0-service \
 	libsensor_shim
 
