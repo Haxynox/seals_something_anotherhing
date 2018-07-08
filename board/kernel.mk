62b6334896c24a33ef60dbfa8dff0911eb1031dd
ivanmeler <i_ivan@windowslive.com>
2018-07-08 01:31:37 -0700

    Prepare device tree for kernel rebase

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,7 +5,3 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := Image
-
-# Kernel Toolchain
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-linaro/bin
--- system_prop.mk
+++ system_prop.mk
@@ -28,22 +28,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
 
-# Hwui
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=88 \
-    ro.hwui.layer_cache_size=58 \
-    ro.hwui.path_cache_size=16 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.shape_cache_size=4 \
-    ro.hwui.gradient_cache_size=2 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=4096 \
-    ro.hwui.text_large_cache_height=2048 \
-    ro.hwui.fbo_cache_size=16
-
 # Network
 # Define default initial receive window size in segments.
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -59,7 +43,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SdcardFS
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.sdcardfs=true
+    ro.sys.sdcardfs=false
 
 # USB
 PRODUCT_PROPERTY_OVERRIDES += \
