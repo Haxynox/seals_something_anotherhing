195a2b9eec44e8cb66c87f02f9b3486d01da9c3c
ivanmeler <i_ivan@windowslive.com>
2018-07-31 13:10:26 -0700

    Update ril configs

--- product/radio.mk
+++ product/radio.mk
@@ -6,9 +6,7 @@ PRODUCT_COPY_FILES += $(DEVICE_PATH)/configs/apns-conf.xml:system/etc/apns-conf.
 # Init Resources
 PRODUCT_PACKAGES += \
 	init.baseband.rc \
-	init.baseband.sh \
-	init.rilchip.rc \
-	init.rilcommon.rc
+	init.baseband.sh
 
 # Packages
 PRODUCT_PACKAGES += \
@@ -18,17 +16,12 @@ PRODUCT_PACKAGES += \
 	android.hardware.radio@1.0 \
 	android.hardware.radio.deprecated@1.0 \
 	modemloader \
-	rild \
-	libreference-ril \
-	libsecril-client-sap \
-	libril 
+	libsecril-client-sap
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.radio.add_power_save=1 \
 	persist.radio.apm_sim_not_pwdn=1 \
-	rild.libpath=/system/lib64/libsec-ril.so \
-	rild.libpath2=/system/lib64/libsec-ril-dsds.so \
 	ro.telephony.default_network=9 \
 	ro.ril.gprsclass=10 \
 	ro.ril.hsxpa=1 \
@@ -39,4 +32,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	telephony.lteOnCdmaDevice=0 \
 	ro.telephony.get_imsi_from_sim=true \
 	ro.ril.force_eri_from_xml=true \
-	net.tethering.noprovisioning=true 
+	net.tethering.noprovisioning=true \
+	ro.debug_level=0x494d \
+	rild.libpath=/system/lib64/libsec-ril.so \
+	rild.libargs=-d /dev/umts_ipc0 \
+	vendor.sec.rild.libpath=/vendor/lib64/libsec-ril.so \
+	vendor.sec.rild.libpath2=/vendor/lib64/libsec-ril-dsds.so
--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -48,22 +48,6 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.rilchip.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= init.rilchip.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.rilcommon.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= init.rilcommon.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.bluetooth.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
