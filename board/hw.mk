426805dea5d901c256b1c79b9adbd151ce75b5eb
ivanmeler <i_ivan@windowslive.com>
2018-08-05 10:19:57 +0000

    Properly declare lineagehw paths
    Change-Id: I34b3e3b55e8ecf319acffbf945db0c619a4f5bf3

--- board/hw.mk
+++ board/hw.mk
@@ -1,3 +1,4 @@
 # Hardware
-BOARD_HARDWARE_CLASS += device/samsung/hero-common/lineagehw
-BOARD_HARDWARE_CLASS += hardware/samsung/lineagehw
+BOARD_HARDWARE_CLASS += \
+	device/samsung/hero-common/lineagehw \
+	hardware/samsung/lineagehw
