a69ac85e1b0e6903da2de28fb59d33b9e5963857
ivanmeler <i_ivan@windowslive.com>
2018-08-11 04:50:27 -0700

    Remove bsp build

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,3 +84,4 @@ BOARD_SECCOMP_POLICY += device/samsung/hero-common/seccomp
 
 # Inherit from the proprietary version
 -include vendor/samsung/hero-common/BoardConfigVendor.mk
+-include vendor/seal/seal/BoardConfigVendor.mk
