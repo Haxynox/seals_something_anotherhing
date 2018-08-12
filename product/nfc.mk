a2228262dd06ee849e622acf63a43c74e01cb090
ivanmeler <i_ivan@windowslive.com>
2018-08-12 01:30:34 -0700

    Update nfc config

--- product/nfc.mk
+++ product/nfc.mk
@@ -5,15 +5,16 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
 	$(DEVICE_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-sec.conf \
 	$(DEVICE_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
-	$(DEVICE_PATH)/configs/nfc/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf
+	$(DEVICE_PATH)/configs/nfc/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
+	$(DEVICE_PATH)/configs/nfc/libnfc-brcm.conf:system/vendor/etc/libnfc-nci.conf \
+	$(DEVICE_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-nci.conf
 
 # Packages
 PRODUCT_PACKAGES += \
 	com.android.nfc_extras \
 	Tag \
+	NfcNci \
 	android.hardware.nfc@1.0-impl
-
-#	NfcNci \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
