c535277e5568905cad4eff65d76f40b882de0f2d
ivanmeler <i_ivan@windowslive.com>
2018-08-19 03:18:46 -0700

    Add stagefright shim to camera

--- board/camera.mk
+++ board/camera.mk
@@ -1,3 +1,7 @@
 # Camera
 BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21 := true
 TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := libcamera_parameters_hero
+
+TARGET_LD_SHIM_LIBS += \
+    /system/lib64/libexynoscamera.so|/system/lib64/libstagefright_shim.so \
+    /system/lib/libexynoscamera.so|/system/lib/libstagefright_shim.so
