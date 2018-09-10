676fd82a5716e3e9ccdf65d4f4ada9c57b78b4b4
ivanmeler <i_ivan@windowslive.com>
2018-09-10 08:50:20 +0000

    Move camera hal to new folder
    Change-Id: Ibb0833e40fe4ff3c925959ba20f908bf7f26a008

--- product/camera.mk
+++ product/camera.mk
@@ -1,7 +1,7 @@
 PRODUCT_PACKAGES += \
 	camera.exynos5 \
-	android.hardware.camera.provider@2.4-impl.hero \
 	libcamhelpr \
+	android.hardware.camera.provider@2.4-impl.hero \
 	android.hardware.camera.provider@2.4-service \
 	Snap \
 	Camera2
