1a992b7a1bc70d7ebd70ab6e635787cb4b1672b0
ivanmeler <i_ivan@windowslive.com>
2018-09-11 09:14:40 +0000

    Revert "Add fingerprint shims till we fix osrc bauth implementation"
    This reverts commit 88054240deb439edd7be50ef2c2eba9f034a0228.

--- board/sensors.mk
+++ board/sensors.mk
@@ -1,5 +1,3 @@
 # Sensors
 TARGET_NO_SENSOR_PERMISSION_CHECK := true
-
-TARGET_LD_SHIM_LIBS += \
-	/system/lib64/hw/fingerprint.vendor.exynos5.so|/vendor/lib64/libbauthtzcommon_shim.so
+TARGET_SEC_FP_HAL_VARIANT := bauth
--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,5 +1,4 @@
 # Packages
 PRODUCT_PACKAGES += \
-	android.hardware.biometrics.fingerprint@2.1-service \
 	fingerprint.exynos5 \
-	libbauthtzcommon_shim
+	android.hardware.biometrics.fingerprint@2.1-service
