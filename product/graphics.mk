6057cf5824b069b49136337052e4d41a172a210d
ivanmeler <i_ivan@windowslive.com>
2018-10-02 17:39:09 +0000

    r22p0: remove unneeded flags and shim
    Change-Id: I24ad521c7140ec62bd504a7535856c6f2b5468aa

--- product/graphics.mk
+++ product/graphics.mk
@@ -6,8 +6,7 @@ PRODUCT_PACKAGES += \
 	android.hardware.graphics.allocator@2.0-service \
 	android.hardware.graphics.composer@2.1-impl \
 	android.hardware.graphics.mapper@2.0-impl \
-	libhwc2on1adapter \
-	libprocname
+	libhwc2on1adapter
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
@@ -22,6 +21,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.bq.gpu_to_cpu_unsupported=1 \
 	ro.opengles.version=196610 \
 	ro.sf.lcd_density=560 \
-	debug.hwui.use_buffer_age=false \
 	debug.hwc.winupdate=1 \
 	debug.hwc.otf=1
