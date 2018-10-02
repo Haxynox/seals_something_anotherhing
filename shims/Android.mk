b4c44667c7c4f3da530a13e1e67570d40aece129
ivanmeler <i_ivan@windowslive.com>
2018-10-02 17:39:09 +0000

    r22p0: remove unneeded flags and shim
    Change-Id: I24ad521c7140ec62bd504a7535856c6f2b5468aa

--- shims/Android.mk
+++ shims/Android.mk
@@ -17,13 +17,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhardware
-LOCAL_SRC_FILES := process_name.cpp
-LOCAL_MODULE := libprocname
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := libhardware libgui libui
 LOCAL_C_INCLUDES := frameworks/native/include
 LOCAL_SRC_FILES := camparams.cpp
