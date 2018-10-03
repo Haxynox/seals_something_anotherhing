8774286917b7679c7fdbd09312e0c0d41434ef04
ivanmeler <i_ivan@windowslive.com>
2018-10-03 15:34:19 +0200

    Remove unused shims

--- product/media.mk
+++ product/media.mk
@@ -2,9 +2,7 @@ DEVICE_PATH := device/samsung/hero-common
 
 # Stagefright
 PRODUCT_PACKAGES += \
-	libstagefright_shim \
-	libExynosOMX_shim \
-	libui_shim
+	libstagefright_shim
 
 # Media profile
 PRODUCT_COPY_FILES += \
@@ -15,13 +13,3 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/media/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml \
 	$(DEVICE_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
 	$(DEVICE_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
-
-# Overriden service definition
-PRODUCT_COPY_FILES += \
-	$(DEVICE_PATH)/configs/init/android.hardware.media.omx@1.0-service.rc:system/vendor/etc/init/android.hardware.media.omx@1.0-service.rc \
-	$(DEVICE_PATH)/configs/init/mediaserver.rc:system/etc/init/mediaserver.rc
-
-# seccomp rules
-PRODUCT_COPY_FILES += \
-	$(DEVICE_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-	$(DEVICE_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
