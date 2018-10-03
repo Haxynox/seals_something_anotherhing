3521e9d6f359656a3370158b56b73a6ba5e102c1
ivanmeler <i_ivan@windowslive.com>
2018-10-03 15:34:19 +0200

    Remove unused shims

--- board/camera.mk
+++ board/camera.mk
@@ -2,8 +2,9 @@
 BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21 := true
 TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := libcamera_parameters_hero
 
-TARGET_LD_SHIM_LIBS += \
-    /system/lib64/libexynoscamera.so|/system/lib64/libstagefright_shim.so \
-    /system/lib/libexynoscamera.so|/system/lib/libstagefright_shim.so \
+
+TARGET_LD_SHIM_LIBS := \
+    /system/lib64/libexynoscamera.so|/system/lib64/libexynoscamera_shim.so \
     /system/lib/libexynoscamera.so|/system/lib/libexynoscamera_shim.so \
-    /system/lib64/libexynoscamera.so|/system/lib64/libexynoscamera_shim.so
+    /system/lib64/libstagefright.so|libstagefright_shim.so \
+    /system/lib/libstagefright.so|libstagefright_shim.so
