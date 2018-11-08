9871d0706f5ee3bd2e20cdd70f1f3e3f6c7911fc
ivanmeler <i_ivan@windowslive.com>
2018-11-08 19:39:32 +0000

    TEMP: Do not include lineagehw from hardware/samsung
    Change-Id: I36c9be9c47507c9a3b676cf36585666771edcf80

--- board/hw.mk
+++ board/hw.mk
@@ -1,6 +1,4 @@
 # Hardware
 
 JAVA_SOURCE_OVERLAYS := \
-    org.lineageos.hardware|hardware/samsung/lineagehw|**/*.java \
     org.lineageos.hardware|device/samsung/hero-common/lineagehw|**/*.java
-    
