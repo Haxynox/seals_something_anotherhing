cb9d300cd397870419035e5547640a152bdf2d26
ivanmeler <i_ivan@windowslive.com>
2018-11-29 16:51:49 +0000

    Revert "TEMP: Do not include lineagehw from hardware/samsung"
    This reverts commit 9871d0706f5ee3bd2e20cdd70f1f3e3f6c7911fc.

--- board/hw.mk
+++ board/hw.mk
@@ -1,4 +1,6 @@
 # Hardware
 
 JAVA_SOURCE_OVERLAYS := \
+    org.lineageos.hardware|hardware/samsung/lineagehw|**/*.java \
     org.lineageos.hardware|device/samsung/hero-common/lineagehw|**/*.java
+    
