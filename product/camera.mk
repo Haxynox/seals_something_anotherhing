b63cf845b3cf1baeadd49292ddb148ba6a381bdd
ivanmeler <i_ivan@windowslive.com>
2018-12-01 09:16:41 +0100

    Remove custom camera implementation

--- product/camera.mk
+++ product/camera.mk
@@ -1,5 +1,7 @@
 PRODUCT_PACKAGES += \
 	libexynoscamera_shim  \
-	android.hardware.camera.provider@2.4-impl.hero \
+	camera.device@3.2-impl \
+	camera.device@1.0-impl \
+	android.hardware.camera.provider@2.4-impl \
 	android.hardware.camera.provider@2.4-service \
 	Camera2
