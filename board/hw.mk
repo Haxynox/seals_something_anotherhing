f7f61a633e94401565f4f2f5255a633d78e85781
ivanmeler <i_ivan@windowslive.com>
2018-12-02 12:13:13 +0100

    hero: implement DT2W

--- board/hw.mk
+++ board/hw.mk
@@ -3,4 +3,6 @@
 JAVA_SOURCE_OVERLAYS := \
     org.lineageos.hardware|hardware/samsung/lineagehw|**/*.java \
     org.lineageos.hardware|device/samsung/hero-common/lineagehw|**/*.java
-    
+
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
